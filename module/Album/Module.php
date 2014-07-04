ca00cd30e726a6690a1cbe50e105c20a02b3e9e6
Waldson Moura <mourawaldson@gmail.com>
2014-07-04 00:36:20 -0400

    adding diagnostics

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -5,6 +5,10 @@ use Album\Model\Album;
 use Album\Model\AlbumTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
+use ZendDiagnostics\Check\DirWritable;
+use ZendDiagnostics\Check\ExtensionLoaded;
+use ZendDiagnostics\Check\ProcessRunning;
+use ZendDiagnostics\Check\PhpVersion;
 
 class Module
 {
@@ -42,6 +46,46 @@ class Module
                     return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
                 },
             ),
+        );
+    }
+
+
+    public function getDiagnostics()
+    {
+        return array(
+            'Cache & Log Directories Available' => function() {
+                $diagnostic = new DirWritable(array(
+                    __DIR__ . '/../../data/cache',
+                    __DIR__ . '/../../data/log'
+                ));
+                return $diagnostic->check();
+            },'Check PHP extensions' => function(){
+                $diagnostic = new ExtensionLoaded(array(
+                    'json',
+                    'pdo',
+                    'pdo_pgsql',
+                    'intl',
+                    'session',
+                    'pcre',
+                    'zlib',
+                    'apc',
+                    'apcu',
+                    'Zend OPcache'
+                ));
+                return $diagnostic->check();
+            },'Check Apache is running' => function(){
+                $diagnostic = new ProcessRunning('apache2');
+                return $diagnostic->check();
+            },'Check PostgreSQL is running' => function(){
+                $diagnostic = new ProcessRunning('postgresql');
+                return $diagnostic->check();
+            },'Check Memcached is running' => function(){
+                $diagnostic = new ProcessRunning('beanstalkd');
+                return $diagnostic->check();
+            },'Check PHP Version' => function(){
+                $diagnostic = new PhpVersion('5.3.0', '>=');
+                return $diagnostic->check();
+            }
         );
     }
 }
